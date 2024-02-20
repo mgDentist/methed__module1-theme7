f10863be902f72f1019d1525db7b15102195a595
mgDentist <gmi1987@gmail.com>
2024-02-20 16:42:08 +0400

    module1-theme7-task03

---
+++ task03.js
@@ -0,0 +1,12 @@
+const names = ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
+const prefixedNames = [];
+
+const addPrefix = (arr, prefix) => {
+    for (let i = 0; i < arr.length; i++) {
+        prefixedNames.push(`${prefix} ${names[i]}`);
+    }
+
+    console.log(prefixedNames);
+};
+
+addPrefix(names, 'Mr');
